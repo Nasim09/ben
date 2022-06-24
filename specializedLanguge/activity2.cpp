4d40b394d495c852572d79da54e26f54558ebf08
Nasim Lawrence <nasimlawrence@The-Hidden-Genius-Project-30.local>
2022-06-24 15:43:20 -0700

    update

---
+++ specializedLanguge/activity2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int x;
+x = 1;
+x = x * 20;
+x = x * 20;
+ cout << "A person with measles may cause ";
+cout <<  x;
+cout << "\n";
+cout << "people to be infected in two weeks.";
+  return 0;
+}
---
+++ specializedLanguge/activity3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+int x = 23;
+cout << x;
+cout << " is a great age.";
+return 0;
+}
